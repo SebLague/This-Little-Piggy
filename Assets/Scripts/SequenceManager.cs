0ab87db17fd807ac06074fe9baa93fb0fd8ea742
Sebastian Lague <sebastlague@gmail.com>
2020-01-15 00:51:48 +0200

    enabled hostile planet

--- Assets/Scripts/SequenceManager.cs
+++ Assets/Scripts/SequenceManager.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SequenceManager : MyBase {
 
@@ -15,60 +15,59 @@ public class SequenceManager : MyBase {
 	public AudioClip portalWarning;
 	public AudioClip portalArrive;
 
-	public override void Awake() {
-		base.Awake();
-		portal.gameObject.SetActive(false);
-		cam.gameObject.SetActive(false);
+	public override void Awake () {
+		base.Awake ();
+		portal.gameObject.SetActive (false);
+		cam.gameObject.SetActive (false);
 		instance = this;
 
-	
 	}
 
-	public static void Warn() {
-		instance.PlayAudio(instance.portalWarning);
+
+
+	public static void Warn () {
+		instance.PlayAudio (instance.portalWarning);
 	}
 
-	public static void ActivatePortal() {
+	public static void ActivatePortal () {
 
-		instance.player.SetActive(false);
-		instance.cam.gameObject.SetActive(true);
-		instance.StartCoroutine("PortalArrive");
-		instance.cam.ActivatePortalIntro();
+		instance.player.SetActive (false);
+		instance.cam.gameObject.SetActive (true);
+		instance.StartCoroutine ("PortalArrive");
+		instance.cam.ActivatePortalIntro ();
 	}
 
-	IEnumerator PortalArrive() {
-		SceneMusicManager.WarTrack();
-		PlayAudio(portalArrive);
-		portal.gameObject.SetActive(true);
-		portalCam2.SetActive(true);
+	IEnumerator PortalArrive () {
+		SceneMusicManager.WarTrack ();
+		PlayAudio (portalArrive);
+		portal.gameObject.SetActive (true);
+		portalCam2.SetActive (true);
 		float scaleV = 0;
 		float scale = 0;
-		spawnEnemy.IntroSpawns();
+		spawnEnemy.IntroSpawns ();
 		while (scale < 1) {
-			scale = Mathf.MoveTowards(scale,1,1 * Time.deltaTime);
+			scale = Mathf.MoveTowards (scale, 1, 1 * Time.deltaTime);
 			//scale = Mathf.Clamp01(scale);
 			portal.localScale = Vector3.one * scale;
 			yield return null;
 		}
 
-
-
 	}
 
-	public static void EnteredPortalIntro() {
-		instance.introEnterCount ++;
+	public static void EnteredPortalIntro () {
+		instance.introEnterCount++;
 		if (instance.introEnterCount >= 2) {
-			instance.Invoke("ResumeGameplay",2);
+			instance.Invoke ("ResumeGameplay", 2);
 		}
 	}
 
-	void ResumeGameplay() {
+	void ResumeGameplay () {
 		Portal.activateCams = true;
-		portalCam1.SetActive(false);
-		portalCam2.SetActive(false);
-		cam.gameObject.SetActive(false);
-		player.SetActive(true);
-		Player.FightBegins();
-		spawnEnemy.StartSpawning();
+		portalCam1.SetActive (false);
+		portalCam2.SetActive (false);
+		cam.gameObject.SetActive (false);
+		player.SetActive (true);
+		Player.FightBegins ();
+		spawnEnemy.StartSpawning ();
 	}
 }
